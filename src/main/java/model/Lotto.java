a5deff8334de66d99b9b5f2b48369598f211bd7a
jngbnss <jngbnmn@naver.com>
2025-12-16 14:54:45 +0900

    feat : 로또 검증 기능 완료

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -1,16 +1,49 @@
 package model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-    private List<Integer> lotto;
+    private static final int LOTTO_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
+    private List<Integer> numbers;
 
     public Lotto(List<Integer> lotto) {
-        this.lotto = lotto;
+        validate(lotto);
+        this.numbers = lotto;
     }
 
-    public List<Integer> getLotto() {
+    private void validate(List<Integer>numbers){
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
+    }
 
-        return lotto;
+    private void validateSize(List<Integer> numbers) {
+        if(numbers.size()!=LOTTO_SIZE){
+            throw new IllegalArgumentException("로또 번호는 6개이어야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> unique = new HashSet<>(numbers);
+        if(unique.size()!=numbers.size()){
+            throw new IllegalArgumentException("로또 번호는 중복될수 없습니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if(number<MIN_NUMBER||number>MAX_NUMBER){
+                throw new IllegalArgumentException("로또 번호의 범위는 1~45입니다.");
+            }
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }
---
+++ src/main/java/model/LottoTest.java
@@ -0,0 +1,36 @@
+package model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+
+    @Test
+    void 로또_번호는_6개여야_한다() {
+        assertThatThrownBy(() ->
+                new Lotto(List.of(1, 2, 3, 4, 5))
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호는_중복될_수_없다() {
+        assertThatThrownBy(() ->
+                new Lotto(List.of(1, 2, 3, 4, 5, 5))
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호는_1부터_45_사이여야_한다() {
+        assertThatThrownBy(() ->
+                new Lotto(List.of(0, 1, 2, 3, 4, 5))
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 정상적인_로또_생성() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+}
